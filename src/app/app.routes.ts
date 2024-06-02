fe707297aa6152c926b5172626d27dcc54cc3f88
troybedingfield <troybedingfield@gmail.com>
2024-06-01 21:09:51 -0700

    init and initial build

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,18 @@
-import { Routes } from '@angular/router';
+import { RouterModule, Routes } from '@angular/router';
+import { PagenotfoundComponent } from './pagenotfound/pagenotfound.component';
+import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+import { NgModule } from '@angular/core';
 
-export const routes: Routes = [];
+
+const routes: Routes = [
+    { path: '', component: HomeComponent },
+    { path: '**', component: PagenotfoundComponent }
+];
+
+@NgModule({
+    imports: [RouterModule.forRoot(routes)],
+    exports: [RouterModule]
+})
+export class AppRoutingModule { }
+
