652460af7839a98271020fb3850a4b276757b637
troybedingfield <troybedingfield@gmail.com>
2024-06-03 11:19:56 -0700

    home background color and top text update

--- src/app/shared/services/contact.service.ts
+++ src/app/shared/services/contact.service.ts
@@ -1,9 +1,22 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { catchError, map } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class ContactService {
 
-  constructor() { }
+  // private api = "email.us-east-2.amazonaws.com";
+
+  constructor(
+    // private http: HttpClient
+  ) { }
+
+  // PostMessage(input: any) {
+  //   return this.http.post(this.api, input, { responseType: 'text' }).pipe(
+  //     map((response: any) => { return response; }),
+  //     catchError((err) => { return err; })
+  //   )
+  // }
 }
