1c10ceb0543ed4c4d3242c52c47f2990507a0c31
troybedingfield <troybedingfield@gmail.com>
2024-06-06 12:23:40 -0700

    load fonts locally font.css, added resume pdf, updated header resume button to view resume

--- src/app/shared/components/header/header.component.ts
+++ src/app/shared/components/header/header.component.ts
@@ -1,10 +1,22 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
   styleUrl: './header.component.scss'
 })
-export class HeaderComponent {
+export class HeaderComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+    //Called after the constructor, initializing input properties, and the first call to ngOnChanges.
+    //Add 'implements OnInit' to the class.
+
+  }
+
+  viewResume() {
+    window.open('./assets/troy-bedingfield.pdf');
+  }
 
 }
