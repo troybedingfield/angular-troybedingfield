24b258fdfbd942f39ff26573a73a561612adb6e0
troybedingfield <troybedingfield@gmail.com>
2024-06-21 17:06:27 -0700

    design component/page added and route setup

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -3,10 +3,12 @@ import { PagenotfoundComponent } from './pagenotfound/pagenotfound.component';
 import { AppComponent } from './app.component';
 import { HomeComponent } from './home/home.component';
 import { NgModule } from '@angular/core';
+import { DesignComponent } from './design/design.component';
 
 
 const routes: Routes = [
     { path: '', component: HomeComponent },
+    { path: 'design', component: DesignComponent },
     { path: '**', component: PagenotfoundComponent }
 ];
 
