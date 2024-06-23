77c63279e06e7dd2bdbdea90968bfc2e113dfd8d
troybedingfield <troybedingfield@gmail.com>
2024-06-23 03:29:37 -0700

    click to open/close and viewchild elementref added to click outside dropdown to close instead of hover

--- src/app/shared/components/split-button/split-button.component.ts
+++ src/app/shared/components/split-button/split-button.component.ts
@@ -1,4 +1,4 @@
-import { Component, EventEmitter, Input, Output, input } from '@angular/core';
+import { Component, ElementRef, EventEmitter, Input, Output, Renderer2, ViewChild, input } from '@angular/core';
 import { NgClass, NgFor, NgIf } from '@angular/common';
 
 @Component({
@@ -11,6 +11,7 @@ import { NgClass, NgFor, NgIf } from '@angular/common';
 export class SplitButtonComponent {
   dropdownValue: string = '';
   isOpen: 'close' | 'open' | undefined = 'close';
+
   @Input() label: string = '';
   @Input() color: 'default' | 'secondary' | 'success' | 'warning' | 'error' | 'disabled' | undefined;
   @Input() disabled = false;
@@ -25,17 +26,56 @@ export class SplitButtonComponent {
   @Output() eventValue = new EventEmitter<any>();
   @Output() buttonClick = new EventEmitter<Event>();
 
+  @ViewChild('dropdownButton') dropdownButton: ElementRef | undefined;
+  @ViewChild('dropdownMenu') dropdownMenu: ElementRef | undefined;
+
+  constructor(private renderer: Renderer2) {
+
+  }
+
+  ngAfterViewInit(): void {
+    //Called after ngAfterContentInit when the component's view has been initialized. Applies to components only.
+    //Add 'implements AfterViewInit' to the class.
+    console.log(this.dropdownButton?.nativeElement)
+    console.log(this.dropdownMenu?.nativeElement)
+    this.renderer.listen('window', 'click', (e: Event) => {
+      /**
+       * Only run when toggleButton is not clicked
+       * If we don't check this, all clicks (even on the toggle button) gets into this
+       * section which in the result we might never see the menu open!
+       * And the menu itself is checked here, and it's where we check just outside of
+       * the menu and button the condition abbove must close the menu
+       */
+      if (e.target !== this.dropdownButton?.nativeElement && e.target !== this.dropdownMenu?.nativeElement) {
+        this.isOpen = 'close';
+
+      }
+    });
+  }
+
   setValue(value: string) {
     this.dropdownValue = value;
     this.isOpen = 'close';
     this.eventValue.emit(value);
+    // this.isMenuOpen = !this.isMenuOpen
   }
 
   changeStyle($event: any) {
-    this.isOpen = $event.type == 'mouseover' ? 'open' : 'close';
+    this.isOpen = $event.type == 'click' ? 'open' : 'close';
   }
 
   buttonClicked(event: any) {
     this.buttonClick.emit(event);
+  }
+
+  splitOpen(event: any) {
+    if (event.currentTarget && this.isOpen === 'open') {
+      this.isOpen = 'close';
+
+    } else {
+      this.isOpen = 'open';
+
+    }
+
   }
 }
