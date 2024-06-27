77eed241cef853cbb2edff2727745bfc2258c481
troybedingfield <troybedingfield@gmail.com>
2024-06-26 17:26:15 -0700

    new hompage-montage image and design page updates

--- src/app/design/design.component.ts
+++ src/app/design/design.component.ts
@@ -1,12 +1,41 @@
+import { NgIf } from '@angular/common';
 import { Component } from '@angular/core';
+import { SharedModule } from '../shared/modules/shared.module';
 
 @Component({
   selector: 'app-design',
   standalone: true,
-  imports: [],
+  imports: [NgIf, SharedModule],
   templateUrl: './design.component.html',
   styleUrl: './design.component.scss'
 })
 export class DesignComponent {
+  showDesktopProto: boolean = false;
+  showTabletProto: boolean = false;
+  showMobileProto: boolean = false;
 
+  showDesktopText: string = 'Show'
+  hideDesktopText: string = 'Hide'
+  showMobileText: string = 'Show'
+  hideMobileText: string = 'Hide'
+
+  constructor() {
+
+  }
+
+  showDesktop() {
+    this.showDesktopProto = !this.showDesktopProto
+    this.showMobileProto = false
+    this.showTabletProto = false
+  }
+  showMobile() {
+    this.showDesktopProto = false
+    this.showMobileProto = !this.showMobileProto
+    this.showTabletProto = false
+  }
+  showTablet() {
+    this.showDesktopProto = false
+    this.showMobileProto = false
+    this.showTabletProto = !this.showTabletProto
+  }
 }
